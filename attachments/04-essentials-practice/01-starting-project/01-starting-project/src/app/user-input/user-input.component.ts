686dafb8f304ac8c8b54332ee6c1c858da110ba5
Gimenez <jgimenez@Almato.com>
2025-01-16 12:27:21 +0100

    Uso de 'services'

--- attachments/04-essentials-practice/01-starting-project/01-starting-project/src/app/user-input/user-input.component.ts
+++ attachments/04-essentials-practice/01-starting-project/01-starting-project/src/app/user-input/user-input.component.ts
@@ -1,6 +1,7 @@
-import { Component, output, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { InvestmentInput } from '../investment-input.model';
+
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -10,20 +11,21 @@ import { InvestmentInput } from '../investment-input.model';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  calculate = output<InvestmentInput>(); //Es una buena práctica poner el tipo de dato que se va a emitir. En este caso, un objeto tipo 'data', como hemos nombrado antes en el .ts del app-component.
   enteredInitialInvestment = signal('0'); //Aquí no hace falta especificar el tipo porque Typescript ya lo lee del predefinido.
   enteredAnnualInvestment = signal('0');
   enteredExpectedReturn = signal ('5');
   enteredDuration = signal('10');
 
+  constructor(private investmentService: InvestmentService) {}
 
   onSubmit() {
-    this.calculate.emit({
+    this.investmentService.calculateInvestmentResults({
       initialInvestment: +this.enteredInitialInvestment(), //si ponemos un '+' delante pasa de string a number.
       duration: +this.enteredDuration(),
       expectedReturn: +this.enteredExpectedReturn(),
       annualInvestment: +this.enteredAnnualInvestment()
     });
+    
     this.enteredInitialInvestment.set('0'); //Con esto seteamos el valor cada vez que pulsamos 'Calculate', es decir, cada vez que hacemos 'submit'.
     this.enteredAnnualInvestment.set('0');
     this.enteredExpectedReturn.set('5');
